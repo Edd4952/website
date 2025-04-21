31f73de266299d7de57704458a1c7335f158da3d
Edd4952 <zilbert3dward@gmail.com>
2025-04-20 21:23:41 -0500

    first real update

--- src/Pages/Movies.js
+++ src/Pages/Movies.js
@@ -1,24 +1,17 @@
 import React from "react";
 import '../App.css';
 import { Link } from 'react-router-dom'
-import fightclub from '../fightclub.jpg';
-import fightclubmovie from './fightclubmovie';
+import Header from "../Components/Header.js";
 
 function Movies(){
   return (
     <div>
-      <header className="top-header">
-        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-          <h1>Edward<span className="antibold">Zilbert</span></h1>
-        </Link>
-      </header>
+      <Header/>
 
       
       <header className="App-header">
         <p>choose the movie to watch:</p>
-        <Link to="/fightclubmovie">
-          <img src={fightclub} alt="Description of the image"/>
-        </Link>
+        
       </header>
       <Link to="/about">About</Link>
     </div>
