adbcbb0093affb0f84abb953bbda24fabedd0e50
Edd4952 <zilbert3dward@gmail.com>
2025-04-22 16:23:19 -0500

    got a lot done with the header, need to fix the resume pdf loader

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
+import "../Assets/github.png";
+import "../Assets/linkedin.png";
+import "../Assets/resume.pdf";
 
 function Header() {
     return (
         <header className="top-header">
             
 
-              <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+              <Link to="/" style={{ textDecoration: 'none', color: 'inherit' , display: 'flex', flexDirection: 'row'}}>
                 <span className="bold">Edward </span>Zilbert
               </Link>
 
@@ -19,6 +22,17 @@ function Header() {
                 <Link to="/contact" className="nav_link">Contact</Link>
                         
               </nav>
+              <div className="socials">
+                <a href="https://www.linkedin.com/in/edward-zilbert-251040216/" target="_blank" rel="noopener noreferrer">
+                  <img src={require("../Assets/linkedin.png")} alt="LinkedIn" className="social-icon" />
+                </a>
+                <a href="https://github.com/Edd4952" target="_blank" rel="noopener noreferrer">
+                  <img src={require("../Assets/github.png")} alt="Github" className="social-icon" />
+                </a>
+                <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+                  <img src={require("../Assets/resumeicon.png")} alt="Resume" className="social-icon" />
+                </a>
+              </div>
 
               
             
