84b784a2e789748b14192001f39be4db7e7ef418
Edd4952 <zilbert3dward@gmail.com>
2025-06-12 16:28:48 -0500

    started projects

--- src/Components/3_Skills.js
+++ src/Components/3_Skills.js
@@ -19,24 +19,32 @@ import gameMaker from "../Assets/gamemaker.png";
 function Skills() {
     const [setExpanded] = useState([]);
     const javasdesc = "JavaScript is a versatile programming language used for web development, server-side scripting, and more.";
-    const reactdesc = "React is a JavaScript library for building user interfaces.";
-    const cssdesc = "CSS is used to style and layout web pages.";
-    const cppdesc = "C++ is a powerful general-purpose programming language that supports object-oriented programming.";
+    const pythonDesc = "Python is a high-level, interpreted programming language known for its readability and versatility. ";
+    const javaDesc = "Java is a widely-used object-oriented programming language. It is the first programming language I learned in highschool.";
+    const reactdesc = "React is a JavaScript library for building user interfaces. I once heard it described as the 'language of the slaves' because it was developed by Facebook in 2013 to accomodate for new developers lack of understanding of JS libraries.";
+    const cssdesc = "CSS is used to style and layout web pages. I have a solid understanding of CSS, and I used it to design this portfolio.";
+    const cppdesc = "C++ is a powerful, high-performance programming language often used for system, software, and game development. I learned many basic and intermediate concepts in college, and continue to study it currently.";
+    const csharpDesc = "C# is a modern, object-oriented programming language developed by Microsoft. I find that it is closest to java.";
+    const vscodeDesc = "This is the most basic and widely used code editor by Microsoft. I used it to make this portfolio";
+    const intellijdesc = "IntelliJ IDEA is an integrated development environment (IDE) for Java development. I used it in highschool to work on Java projects.";
+    const unitydesc = "Unity is a cross-platform game engine used for developing video games and simulations. I learned many of the basics including c#.";
+    const gameMakerDesc = "GameMaker Studio is a game development platform that allows users to create 2D games using its own scripting language, GML. I used it to make a 2D space shooter game in highschool.";
+    const gmlDesc = "GameMaker Language (GML) is a high-level programming language used in GameMaker Studio for game development. I fidn that its like a mix of Java/C# and python.";
 
     const languages = [
         { id: 1, title: "Javascript", description: javasdesc, image: js },
         { id: 2, title: "React", description: reactdesc, image: react },
         { id: 3, title: "CSS", description: cssdesc, image: css },
-        { id: 4, title: "C++", description: cppdesc, image: cpp },
-        { id: 5, title: "Python", description: "Python is a high-level, interpreted programming language known for its readability.", image: python },
-        { id: 6, title: "Java", description: "Java is a widely-used object-oriented programming language.", image: java },
-        { id: 7, title: "GML", description: "", image: gameMaker },
+        { id: 4, title: "C++", description: cppdesc, image: cpp },        
+        { id: 5, title: "Python", description: pythonDesc, image: python },
+        { id: 6, title: "Java", description: javaDesc, image: java },
+        { id: 7, title: "GML", description: gmlDesc, image: gameMaker },
     ];
     const compsciSkills = [
-        { id: 1, title: "VSCode", description: "", image: vsCode },
-        { id: 2, title: "IntelliJ Idea", description: javasdesc, image: intelliJ },
-        { id: 3, title: "Unity", description: javasdesc, image: unity },
-        { id: 4, title: "GameMaker Studio", description: javasdesc, image: gameMaker },
+        { id: 1, title: "VSCode", description: vscodeDesc, image: vsCode },
+        { id: 2, title: "IntelliJ Idea", description: intellijdesc, image: intelliJ },
+        { id: 3, title: "Unity", description: unitydesc, image: unity },
+        { id: 4, title: "GameMaker Studio", description: gameMakerDesc, image: gameMaker },
     ];
     const softSkills = [
         { id: 1, title: "Communication", description: "Effective communication skills are essential for collaboration and teamwork." },
@@ -68,13 +76,11 @@ function Skills() {
                     <div style={{ flexBasis: "100%", height: "0"}}></div>
                     
                     {languages.map((language) => (
-                        <div key={language.id} className="skill-box">
-                            <div className="skill-header">
-                                <h3>{language.title}</h3>
-                                <img src={language.image} alt="" height="20em" width="auto" style={{ verticalAlign: "middle" }} />
-                                <div className="skill-tooltip" style={{left: "0%", transform: "translateX(0%)"}}>
-                                    <p>{language.description}</p>
-                                </div>
+                        <div key={language.id} className="skill-header">    
+                            <h3>{language.title}</h3>
+                            <img src={language.image} alt="" height="20em" width="auto" style={{ verticalAlign: "middle" }} />
+                            <div className="skill-tooltip" style={{left: "0%", transform: "translateX(0%)"}}>
+                                <p>{language.description}</p>
                             </div>
                         </div>
                     ))}                    
@@ -86,13 +92,11 @@ function Skills() {
                     {/*break into next line*/}
                     <div style={{ flexBasis: "100%", height: "0" }}></div>
                     {compsciSkills.map((skill) => (
-                        <div key={skill.id} className="skill-box">
-                            <div className="skill-header">
-                                <h3>{skill.title}</h3>
-                                <img src={skill.image} alt="" height="20em" width="auto" style={{ verticalAlign: "middle" }} />
-                                <div className="skill-tooltip" style={{left: "50%", transform: "translateX(-50%)"}}>
-                                    <p>{skill.description}</p>
-                                </div>
+                        <div key={skill.id} className="skill-header">
+                            <h3>{skill.title}</h3>
+                            <img src={skill.image} alt="" height="20em" width="auto" style={{ verticalAlign: "middle" }} />
+                            <div className="skill-tooltip" style={{left: "50%", transform: "translateX(-50%)"}}>
+                                <p>{skill.description}</p>
                             </div>
                         </div>
                     ))}
@@ -104,14 +108,12 @@ function Skills() {
                     {/*break into next line*/}
                     <div style={{ flexBasis: "100%", height: "0" }}></div>
 
-                    {softSkills.map((skill) => (
-                        <div key={skill.id} className="skill-box">
-                            <div className="skill-header">
-                                <h3>{skill.title}</h3>
-                                <img src={skill.image} alt="" height="20em" width="auto" style={{ verticalAlign: "middle" }} />
-                                <div className="skill-tooltip" style={{left: "100%", transform: "translateX(-100%)"}}>
-                                    <p>{skill.description}</p>
-                                </div>
+                    {softSkills.map((softSkill) => (
+                        <div key={softSkill.id} className="skill-header">
+                            <h3>{softSkill.title}</h3>
+                            <img src={softSkill.image} alt="" height="20em" width="auto" style={{ verticalAlign: "middle" }} />
+                            <div className="skill-tooltip" style={{left: "100%", transform: "translateX(-100%)"}}>
+                                <p>{softSkill.description}</p>
                             </div>
                         </div>
                     ))}
--- src/Components/4_Projects.js
+++ src/Components/4_Projects.js
@@ -2,22 +2,36 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../Styles/App.css";
 import "../Styles/4_Projects.css";
+import spacepreview from "../Assets/spaceshipgame-preview.png"; 
 
 function Projects() {
     const projects = [
-        { id: 1, title: "Project One", description: "Description of project one." , tools: ["React", "Node.js", "CSS"] },
-        { id: 2, title: "Project Two", description: "Description of project two." },
-        { id: 3, title: "Project Three", description: "Description of project three." },
+        { id: 1, title: "Gamemaker Project: Spaceship Game", description: "A 2d game made from scratch with complete movement, collision detection, enemy AI, and basic game logic.", tools: ["Gamemaker Studio", "GML code", "Sprite Maker"], image: spacepreview },
+        { id: 2, title: "Project Two", description: "Description of project two.", tools: ["JavaScript", "HTML", "CSS"], image: spacepreview },
+        { id: 3, title: "Project Three", description: "Description of project three.", tools: ["Python", "Django"], image: spacepreview },
     ];
+    
     return (
         <div>
             <section id="4_Projects"></section>
             <h1 style={{ paddingLeft: "20px", paddingTop: "90px" }}>My Projects</h1>
             <div className="projects-container">{
-                    projects.map(project => (
+                projects.map(project => (
                     <div key={project.id} className="project-box">
-                        <h3>{project.title}</h3>
-                        <p>{project.description}</p>
+                        <div className="project-overview">
+                            {/* All the info for the project */}
+                            <h3>{project.title}</h3>
+                            <p>{project.description}</p>
+                            <div className="tool-container">
+                                {project.tools.map(tool => (
+                                    <div className="tool">{tool}</div>
+                                ))}
+                            </div>
+                        </div>
+                        {/* Project preview, e.g., an image or video */}
+                        <div className="project-preview">
+                            <img src={project.image} alt="" height="200em" width="auto" style={{ verticalAlign: "middle" }} />
+                        </div>
                     </div>
                 ))}
             </div>
